0f61e4bfb7f808a73b3aff2593f54b6d75034b6e
John Goodleaf <jgoodleaf@hamotoconsulting.com>
2018-12-07 13:53:40 -0800

    Added escapes to parens

--- flaskr/app.py
+++ flaskr/app.py
@@ -3,15 +3,16 @@ import os
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app(*args, **kwargs):
     # create and configure the app
+    print(args)
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
-    if test_config is None:
+    if kwargs.get('test_config') is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
